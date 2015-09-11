b1ee003b34dba80ccf9897d57d7fd3b02d51fc3d
Ronando Long <ronando.long@gmail.com>
2015-09-10 17:55:13 -0700

    Confirmed on another letter that it will return correct score

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,4 +5,8 @@ describe("String#scrabble") do
   it("returns a scrabble score for a letter") do
     expect(("a").scrabble()).to(eq(1))
   end
+
+  it("returns a scrabble score for a letter") do
+    expect(("q").scrabble()).to(eq(10))
+  end
 end
